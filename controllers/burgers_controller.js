43858734cc3a99dd6eb21374cf4b57c9d50c91fb
Gary Estime <visionzbg@yahoo.com>
2018-04-24 00:48:32 -0400

    Syntax on burger_controller.js

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -13,7 +13,7 @@ var burger = require("../models/burger.js");
 
 // Create all our routes and set up logic within those routes where required.
 router.get("/", function(req, res) {
-  burger.all(function(data) {
+  burgers.all(function(data) {
     var hbsObject = {
       burgers: data
     };
