a91fe6f946ad76c7ec5c0ba8730e4c3c91da0bac
Gary Estime <visionzbg@yahoo.com>
2018-04-23 20:28:02 -0400

    Initial Setup

---
+++ config/connection.js
@@ -0,0 +1,3 @@
+// * Inside the `connection.js` file, setup the code to connect Node to MySQL.
+
+//    * Export the connection.
---
+++ server.js
@@ -0,0 +1,3 @@
+//Require the following npm packages inside of the server.js file:
+// * express
+// * body-parser
